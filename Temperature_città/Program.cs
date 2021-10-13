0ce8d06d7c3f49b49d5a6c2f42492213e7572889
mattia-gallinaro <72882041+mattia-gallinaro@users.noreply.github.com>
2021-10-13 18:13:23 +0200

    Commenti

--- Temperature_città/Program.cs
+++ Temperature_città/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//nickname: matgal
+//Data: 13/10/2021
+/* Dopo aver acquisito da tastiera una serie di dati relativi alla misurazione della temperatura di una certa città, 
+ * scrivere il codice di un programma(OOP) in C# che determini e visualizzi la temperatura più bassa e quella più alta.*/
+using System;
 using System.Collections.Generic;
 
 namespace Temperature_città
@@ -16,18 +20,21 @@ namespace Temperature_città
             max = -9999;
         }
         //metodi
+        //
         public void setTemp()
         {
             t = int.Parse(Console.ReadLine());
             minimo();
             massimo();
         }
+        //se la temperatura inserita è minore di min allora t viene assegnata a min
         public void minimo()
         {
             if (min > t)
                 min = t;
-        
+
         }
+        //se la temperatura inserita è maggiore di max allora t viene assegnata a max
         public void massimo()
         {
             if (max < t)
@@ -35,14 +42,20 @@ namespace Temperature_città
                 max = t;
 
         }
+        //ritorna min
         public int mostraMinimo()
         {
             return min;
         }
+        //ritorna max
         public int mostraMassimo()
         {
             return max;
         }
+        //l'utente indica quante temperature deve inserire
+        //il for si ripete per la quantità indicata dall'utente
+        //e ad ogni ciclo l'utente inserisce una temperatura che viene contrallata nel caso in cui sia quella minore o quella maggiore
+        //alla fine mostra la temperatura massima e minima inserita dall'utente
         static void Main(string[] args)
         {
             int quantità = 0;
